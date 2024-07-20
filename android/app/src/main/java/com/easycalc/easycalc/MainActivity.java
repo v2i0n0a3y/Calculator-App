891fef40dae73ccb7058c9bb9f1eb99f2522e092
vinay <vinaybhuwad170@gmail.com>
2024-07-20 11:47:43 +0530

    Finish project Calculator

---
+++ android/app/src/main/java/com/easycalc/easycalc/MainActivity.java
@@ -0,0 +1,6 @@
+package com.easycalc.easycalc;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'calculator_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CalculatorView(),
+    );
+  }
+}
